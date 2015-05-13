484b1ee1a661c1aa8b2bf9778fc154d72940471d
Adi Tanase <ady00t@live.com>
2015-05-13 20:50:12 +0200

    first init

---
+++ MyApp/src/MyClass.java
@@ -0,0 +1,15 @@
+
+public class MyClass {
+ 
+	
+	 public static void main(String[] args){
+		 testMethodOne(10, 30);
+	 }
+	 
+    public static double testMethodOne(int a, int b) {
+        return a/b;
+    }
+ 
+}
+
+   
